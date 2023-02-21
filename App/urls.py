bb591decebda6c52e76b311d94b6ca65ca8ab39d
Subham Kumar Singh <shubham26600@gmail.com>
2023-02-21 23:05:57 +0530

    normal push

---
+++ App/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/',views.loginPage,name="login"),
+    path('register/',views.registerPage,name="register")
+]
--- App/views.py
+++ App/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'App/home.html')
+
+def loginPage(request):
+    return render(request,'App/login.html')
+
+def registerPage(request):
+    return render(request,'App/register.html')
